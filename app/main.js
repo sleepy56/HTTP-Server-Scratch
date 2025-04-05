e79ee2cfd252db548eb6ac2b8f962293aa42e3ce
Sleepy <sanketapatil178@gmail.com>
2025-04-05 17:06:02 +0530

    Console

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ const net = require("net");
 const server = net.createServer((socket) => {
     
     socket.on("data", (data)=>{
-        console.log(data);
+        console.log(data.toString);
         const p = data.toString().split(" ")[1];
         if(p === '/'){
             socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
