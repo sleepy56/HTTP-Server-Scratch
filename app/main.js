5857c42095bb61a343021c4c5f9d336c2d077a95
Sleepy <sanketapatil178@gmail.com>
2025-04-05 15:56:41 +0530

    Http status 200 2

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write('HTTP/1.1 200 OK');
+    socket.write('HTTP/1.1 200 OK\r\n\n');
     socket.on("close", () => {
         socket.end();
     });
