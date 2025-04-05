6de7dcd3d8e5ea629ecb1643c6df9cce0569d80e
Sleepy <sanketapatil178@gmail.com>
2025-04-05 16:33:25 +0530

    Extract path test 1

--- app/main.js
+++ app/main.js
@@ -1,8 +1,16 @@
+const { get } = require("http");
 const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    
+    socket.on("data", (data)=>{
+        console.log(data);
+        const p = data.toString().split(" ")[1];
+        const res = p === '/' ? '200 OK' : "404 NOT FOUND";
+        socket.write(`HTPP/1.1 ${res} \r\n\r\n`);
+    });
+
     socket.on("close", () => {
         socket.end();
     });
