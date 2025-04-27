a4ea0d0abb7f87cc8a0f58bf9c277992055eb4a6
Sleepy <sanketapatil178@gmail.com>
2025-04-27 16:04:09 +0530

    Return a file

--- app/main.js
+++ app/main.js
@@ -1,33 +1,63 @@
-const { get } = require("http");
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
 
+const args = process.argv.slice(2);
+const directoryIndex = args.indexOf("--directory");
+const baseDirectory = (directoryIndex !== -1 && args[directoryIndex + 1]) ? args[directoryIndex + 1] : null;
+
+if (!baseDirectory) {
+    console.error("Error: --directory flag is required");
+    process.exit(1);
+}
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    
-    socket.on("data", (data)=>{
-        console.log(data.toString());
+    socket.on("data", (data) => {
         const d = data.toString();
         const headers = d.split('\r\n');
         const url = headers[0].split(" ")[1];
-        if(url === '/'){
+        if (url === '/') {
             socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
-        }
-        else if(url.includes('/echo')){
-            const content = url.split('/echo/')[1];
+            socket.end();
+        } else if (url.startsWith('/echo/')) {
+            const content = url.slice('/echo/'.length);
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
-        }
-        else if(url === '/user-agent'){
-            const content = headers.find((s)=> s.startsWith("User-Agent")).split(" ")[1];
+            socket.end();
+        } else if (url === '/user-agent') {
+            const userAgentHeader = headers.find((s) => s.startsWith("User-Agent"));
+            const content = userAgentHeader ? userAgentHeader.split(": ")[1] : "";
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
-        }
-        else{
+            socket.end();
+        } else if (url.startsWith('/files')) {
+            const filename = url.slice('/files/'.length);
+            const filepath = path.join(baseDirectory,filename);
+            fs.readFile(`./${filepath}`,(err, content)=>{
+                if(err){
+                    console.log(err);
+                    socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+                }
+                else{
+                    socket.write(
+                        "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: application/octet-stream\r\n" +
+                        `Content-Length: ${content.length}\r\n` +
+                        "\r\n"
+                    );
+                    socket.write(content);
+                }
+                socket.end();
+            })
+        } else {
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+            socket.end();
         }
     });
 
-    socket.on("close", () => {
-        socket.end();
+    socket.on("error", (err) => {
+        console.error(`Socket error: ${err}`);
     });
 });
 //
-server.listen(4221, "localhost");
+server.listen(4221, "localhost",()=>{
+    console.log('Listening on port 4221');
+});
